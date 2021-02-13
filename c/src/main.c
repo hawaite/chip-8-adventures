f63e1e14745ffdc82f123a1836948b935b45b893
Helier Waite <helierawaite@gmail.com>
2021-02-13 01:50:51 +0000

    replaced v registers with an array, initialized registers, implemented a bunch of opcodes, and removed RAM access from opcodes that do not require it.

--- c/src/main.c
+++ c/src/main.c
@@ -1,27 +1,13 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct registers{
     uint16_t PC;
     uint16_t I;
     uint8_t SP;
-    uint8_t v0;
-    uint8_t v1;
-    uint8_t v2;
-    uint8_t v3;
-    uint8_t v4;
-    uint8_t v5;
-    uint8_t v6;
-    uint8_t v7;
-    uint8_t v8;
-    uint8_t v9;
-    uint8_t vA;
-    uint8_t vB;
-    uint8_t vC;
-    uint8_t vD;
-    uint8_t vE;
-    uint8_t vF;
+    uint8_t v[16];
 } REGISTERS;
 
 // all instructions are 2 bytes so grab 2 bytes from RAM and bump PC by 2
@@ -35,48 +21,64 @@ uint16_t fetch(REGISTERS* registers, uint8_t* ram){
 }
 
 void execute_0(uint16_t instruction, REGISTERS* registers, uint8_t* ram){
+    switch(instruction){
+        case 0x00E0:
+            // Clear screen
+            break;
+        case 0x00EE:
+            // return from subrouting
+            break;
+    }
 }
 
-void execute_1(uint16_t nnn, REGISTERS* registers, uint8_t* ram){
+void execute_1(uint16_t nnn, REGISTERS* registers){
+    // JUMP - set PC to nnn
+    registers->PC = nnn;
 }
 
 void execute_2(uint16_t nnn, REGISTERS* registers, uint8_t* ram){
 }
 
-void execute_3(uint8_t x, uint8_t kk, REGISTERS* registers, uint8_t* ram){
+void execute_3(uint8_t x, uint8_t kk, REGISTERS* registers){
 }
 
-void execute_4(uint8_t x, uint8_t kk, REGISTERS* registers, uint8_t* ram){
+void execute_4(uint8_t x, uint8_t kk, REGISTERS* registers){
 }
 
-void execute_5(uint8_t x, uint8_t y, REGISTERS* registers, uint8_t* ram){
+void execute_5(uint8_t x, uint8_t y, REGISTERS* registers){
 }
 
-void execute_6(uint8_t x, uint8_t kk, REGISTERS* registers, uint8_t* ram){
+void execute_6(uint8_t x, uint8_t kk, REGISTERS* registers){
+    // SET register x to kk
+    registers->v[x] = kk;
 }
 
-void execute_7(uint8_t x, uint8_t kk, REGISTERS* registers, uint8_t* ram){
+void execute_7(uint8_t x, uint8_t kk, REGISTERS* registers){
+    // ADD kk to current value of v[x]
+    registers->v[x] = registers->v[x] + kk;
 }
 
-void execute_8(uint8_t x, uint8_t y, REGISTERS* registers, uint8_t* ram){
+void execute_8(uint8_t x, uint8_t y, REGISTERS* registers){
 }
 
-void execute_9(uint8_t x, uint8_t y, REGISTERS* registers, uint8_t* ram){
+void execute_9(uint8_t x, uint8_t y, REGISTERS* registers){
 }
 
-void execute_a(uint16_t nnn, REGISTERS* registers, uint8_t* ram){
+void execute_a(uint16_t nnn, REGISTERS* registers){
+    // SET I to nnn
+    registers->I = nnn;
 }
 
 void execute_b(uint16_t nnn, REGISTERS* registers, uint8_t* ram){
 }
 
-void execute_c(uint8_t x, uint8_t kk, REGISTERS* registers, uint8_t* ram){
+void execute_c(uint8_t x, uint8_t kk, REGISTERS* registers){
 }
 
 void execute_d(uint8_t x, uint8_t y, REGISTERS* registers, uint8_t* ram){
 }
 
-void execute_e(uint8_t x, REGISTERS* registers, uint8_t* ram){
+void execute_e(uint8_t x, REGISTERS* registers){
 }
 
 void execute_f(uint8_t x, REGISTERS* registers, uint8_t* ram){
@@ -95,46 +97,46 @@ void decode_and_execute(uint16_t instruction, REGISTERS* registers, uint8_t* ram
             execute_0(instruction, registers, ram);
             break;
         case 0x1: // 1nnn
-            execute_1(nnn, registers, ram);
+            execute_1(nnn, registers);
             break;
         case 0x2: // 2nnn
             execute_2(nnn, registers, ram);
             break;
         case 0x3: // 3xkk
-            execute_3(x, kk, registers, ram);
+            execute_3(x, kk, registers);
             break;
         case 0x4: // 4xkk
-            execute_4(x, kk, registers, ram);
+            execute_4(x, kk, registers);
             break;
         case 0x5: // 5xy0
-            execute_5(x, y, registers, ram);
+            execute_5(x, y, registers);
             break;
         case 0x6: // 6xkk
-            execute_6(x, kk, registers, ram);
+            execute_6(x, kk, registers);
             break;
         case 0x7: // 7xkk
-            execute_7(x, kk, registers, ram);
+            execute_7(x, kk, registers);
             break;
         case 0x8: // 8xy0 8xy1 8xy2 8xy3 8xy4 8xy5 8xy6 8xy7 8xyE
-            execute_8(x, y, registers, ram);
+            execute_8(x, y, registers);
             break;
         case 0x9: // 9xy0
-            execute_9(x, y, registers, ram);
+            execute_9(x, y, registers);
             break;
         case 0xA: // Annn
-            execute_a(nnn, registers, ram);
+            execute_a(nnn, registers);
             break;
         case 0xB: // Bnnn
             execute_b(nnn, registers, ram);
             break;
         case 0xC: // Cxkk
-            execute_c(x, kk, registers, ram);
+            execute_c(x, kk, registers);
             break;
         case 0xD: // Dxyn
             execute_d(x, y, registers, ram);
             break;
         case 0xE: // Ex9E ExA1
-            execute_e(x, registers, ram);
+            execute_e(x, registers);
             break;
         case 0xF: // Fx07 Fx0A Fx15 Fx18 Fx1E Fx29 Fx33 Fx55 Fx65
             execute_f(x, registers, ram);
@@ -142,10 +144,19 @@ void decode_and_execute(uint16_t instruction, REGISTERS* registers, uint8_t* ram
     }
 }
 
+void init_registers(REGISTERS* registers){
+    memset(registers->v, 0, 16);
+    registers->I = 0;
+    registers->PC = 0;
+    registers->SP = 0;
+}
+
 // Implement 00E0, 1NNN, 6XNN, 7XNN, ANNN, DXYN for IBM logo test
 int main(int argc, char* argv[]){
     REGISTERS registers;
-    uint8_t ram[4096];
+    uint8_t ram[4096] = { 0 };
+
+    init_registers(&registers);
 
     //put test value in RAM
     ram[0x1F1] = 0xBE;
@@ -159,4 +170,9 @@ int main(int argc, char* argv[]){
     printf("PC After : %04X\n", registers.PC);
 
     decode_and_execute(0xFABE, &registers, ram);
+
+    printf("v[2] before : %02X\n", registers.v[0x02]);
+    execute_7(0x02, 0x22, &registers);
+    printf("v[2] after : %02X\n", registers.v[0x02]);
+
 }
